493e86918c861bf80d3ca200aabbb60a6e7b22b9
Lea1dev <125105950+Lea1dev@users.noreply.github.com>
2023-03-17 11:57:57 -0300

    pequenas alterações.

--- roboStatus.js
+++ roboStatus.js
@@ -1,4 +1,8 @@
-addEventListener(this, function(){
-    const controle = this.document.getElementsByClassName("controle-ajuste")
-    this.alert(controle);
-})
+const botoes = document.getElementsByClassName("controle-ajuste");
+var inputs = this.document.getElementsByClassName("controle-contador");
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].addEventListener('click', function() {
+        alert(seila);
+    });
+  }
