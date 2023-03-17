bd4df44cb117fc58673d5d86766d3c18c4571bcc
JHONATAN ERIK DOS SANTOS SILVA <2021325119@IFAL-MACEIO.INTRANET>
2023-03-17 15:42:26 -0500

    alteração no roboStatus

--- roboStatus.js
+++ roboStatus.js
@@ -1,23 +1,35 @@
-const botoes = document.getElementsByClassName("decrescer-controle-ajuste");
-const entradas = document.getElementsByClassName("controle-contador")
+const minusButton = document.getElementsByClassName("decrescer-controle-ajuste");
+const plusButton = document.getElementsByClassName("acrescer-controle-ajuste");
+const entradas = document.getElementsByClassName("controle-contador");
 
-for (let i = 0; i < botoes.length; i++) {
-  switch (i) {
-    // case 0:
-    //   botoes[i].addEventListener("click", () => {
-    //     document.getElementById("entrada").value = "Valor 1";
-    //   });
-    //   break;
-    // case 1:
-    //   botoes[i].addEventListener("click", () => {
-    //     document.getElementById("entrada").value = "Valor 2";
-    //   });
-    //   break;
-    
-    default:    
-        botoes[i].addEventListener("click", () => {
-            entradas[i].value = "Valor 1";
-          });
-      break;
+for (let i = 0; i < minusButton.length; i++) {
+
+  minusButton[i].addEventListener("click", () => {
+    entradas[i].value = parseInt(decrescer(entradas[i].value));
+  });
+}
+
+for (let i = 0; i < minusButton.length; i++) {
+
+  plusButton[i].addEventListener("click", () => {
+    entradas[i].value = parseInt(acrescer(entradas[i].value));
+  });
+}
+
+
+function acrescer(valorAtual) {
+  if (valorAtual >= parseInt(0) && valorAtual < parseInt(100)) {
+    return parseInt(valorAtual) + parseInt(10);
   }
+
+  return "00";
+}
+
+function decrescer(valorAtual) {
+  valorAtual = Number(valorAtual);
+
+  if (valorAtual == 0) return 0;
+  return parseInt(valorAtual) - parseInt(10);
+
+
 }
