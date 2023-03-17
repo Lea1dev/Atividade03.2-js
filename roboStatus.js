55cd14d0c95e24ec98e5845f7a7a5ed5006cedf2
JHONATAN ERIK DOS SANTOS SILVA <2021325119@IFAL-MACEIO.INTRANET>
2023-03-17 16:40:16 -0300

    algumas alterações

--- roboStatus.js
+++ roboStatus.js
@@ -1,9 +1,23 @@
-const botao = document.getElementById("botao")
 const botoes = document.getElementsByClassName("decrescer-controle-ajuste");
-// const decrescerBotao = document.getElementsByClassName("decrescer-controle-ajuste");
-// const acrescerBotao = document.getElementsByClassName("decrescer-controle-ajuste");
+const entradas = document.getElementsByClassName("controle-contador")
 
-botoes[0].addEventListener("click", () =>{
-    document.getElementById("entrada").value = 1
-})
-
+for (let i = 0; i < botoes.length; i++) {
+  switch (i) {
+    // case 0:
+    //   botoes[i].addEventListener("click", () => {
+    //     document.getElementById("entrada").value = "Valor 1";
+    //   });
+    //   break;
+    // case 1:
+    //   botoes[i].addEventListener("click", () => {
+    //     document.getElementById("entrada").value = "Valor 2";
+    //   });
+    //   break;
+    
+    default:    
+        botoes[i].addEventListener("click", () => {
+            entradas[i].value = "Valor 1";
+          });
+      break;
+  }
+}
